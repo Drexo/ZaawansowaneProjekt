bc0532efc50f803a36872966502bb19cb03e7088
Kuba <kubaz11011@gmail.com>
2023-06-30 12:22:00 +0200

    feat: add final model, add csv data, add main function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import model
+import pandas as pd
 from fastapi import FastAPI
 from datetime import datetime
 
@@ -11,10 +12,26 @@ def get_aktualna_data():
     return {"aktualna_data": aktualna_data}
 
 @app.post("/predict")
-def predict_price(data):
-    value = model.predict([[
-        data.kolumna1,
-        data.kolumna2,
-        data.kolumna3,
-    ]])
-    return value[0]
+def predict_price(age: int, sex: str, race: str, educationnum: int, workclass: str, occupation: str, maritalstatus: str, hoursperweek: int):
+    teraz = [age, sex, race, educationnum, workclass, occupation, maritalstatus, hoursperweek]
+    df = pd.read_csv("csv.csv", sep=",", engine="python")
+    df.loc[(len(df) - 1), "age"] = teraz[0]
+    df.loc[(len(df) - 1), "sex"] = teraz[1]
+    df.loc[(len(df) - 1), "race"] = teraz[2]
+    df.loc[(len(df) - 1), "education-num"] = teraz[3]
+    df.loc[(len(df) - 1), "workclass"] = teraz[4]
+    df.loc[(len(df) - 1), "occupation"] = teraz[5]
+    df.loc[(len(df) - 1), "marital-status"] = teraz[6]
+    df.loc[(len(df) - 1), "hours-per-week"] = teraz[7]
+    X_new = df[['age', 'sex', 'race', 'education-num', 'workclass', 'occupation', 'marital-status', 'hours-per-week']]
+    X_new_dummies = pd.get_dummies(data=X_new)
+    nowy = X_new_dummies.tail(1)
+    value = model.predict(nowy)
+
+    for col in value:
+        wynik = value
+
+    if (wynik == [0]):
+        return 'biedny'
+    else:
+        return 'bogaty'
